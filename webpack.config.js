ac7b80edb349e0344be8780bc40d39dc31a5f2cc
Titouan Galopin <galopintitouan@gmail.com>
2017-09-17 19:40:38 +0200

    Fix social sharer

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var Encore = require('@symfony/webpack-encore');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 var HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
     .setOutputPath('public/')
@@ -12,38 +13,44 @@ Encore
     .enablePreactPreset()
     .enableVersioning(Encore.isProduction())
     .enableSourceMaps(!Encore.isProduction())
-	.addPlugin(new HtmlWebpackPlugin({
-		template: 'src/index.ejs',
-		alwaysWriteToDisk: true
-	}))
-	.addPlugin(new HtmlWebpackHarddiskPlugin())
-	.addPlugin(new FaviconsWebpackPlugin({
-		logo: './src/images/favicon.png',
-		prefix: 'meta-icons-[hash]/',
-		emitStats: false,
-		statsFilename: 'iconstats-[hash].json',
-		persistentCache: true,
-		inject: true,
-		background: '#fff',
-		title: 'Simule ta taxe',
-		icons: {
-			android: true,
-			appleIcon: true,
-			favicons: true,
-			firefox: true,
-			appleStartup: false,
-			coast: false,
-			opengraph: false,
-			twitter: false,
-			yandex: false,
-			windows: false
-		}
-	}))
     .enablePostCssLoader((options) => {
         options.config = {
             path: 'postcss.config.js'
         };
     })
+    .addPlugin(new HtmlWebpackPlugin({
+        template: 'src/index.ejs',
+        alwaysWriteToDisk: true
+    }))
+    .addPlugin(new HtmlWebpackHarddiskPlugin())
+    .addPlugin(new CopyWebpackPlugin([
+        {
+            from: 'src/images/th-social-media.jpg',
+            to: 'images/th-social-media.jpg',
+        },
+    ]))
+    .addPlugin(new FaviconsWebpackPlugin({
+        logo: './src/images/favicon.png',
+        prefix: 'meta-icons-[hash]/',
+        emitStats: false,
+        statsFilename: 'iconstats-[hash].json',
+        persistentCache: true,
+        inject: true,
+        background: '#fff',
+        title: 'Simule ta taxe',
+        icons: {
+            android: true,
+            appleIcon: true,
+            favicons: true,
+            firefox: true,
+            appleStartup: false,
+            coast: false,
+            opengraph: false,
+            twitter: false,
+            yandex: false,
+            windows: false
+        }
+    }))
 ;
 
 module.exports = Encore.getWebpackConfig();
